d5ef306dd70ef3b4e97eb08fc254533105029036
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-05-22 12:17:47 +0530

    Blog Website Update

---
+++ Blog Website/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const dotenv = require("dotenv");
+const app = express()
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("DB Connection Established");
+    });
+
+app.set("views", "./view")
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(3000)
